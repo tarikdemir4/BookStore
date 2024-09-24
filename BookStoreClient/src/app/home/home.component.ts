579e0e1192c823db2f12cb10ef68593f9df45820
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-09-25 01:17:05 +0300

    Search kısmı backend ve frontend 'te tamamlandı.

--- BookStoreClient/src/app/home/home.component.ts
+++ BookStoreClient/src/app/home/home.component.ts
@@ -3,23 +3,28 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { RequestModel } from '../models/request.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, TranslateModule, CommonModule],
+  imports: [RouterModule, TranslateModule, CommonModule,FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   response: any;
   pageNumbers: number[] = [];
+  request: RequestModel = new RequestModel();
 
   constructor(private http: HttpClient) {
     this.getAll();
   }
   getAll(pageNumber = 1) {
-    this.http.get(`https://localhost:7280/${pageNumber}/10`)
+    this.request.pageNumber = pageNumber;
+    this.http
+      .post(`https://localhost:7280/api/Books/GetAll`, this.request)
       .subscribe(res => {
         this.response = res;
         this.setPageNumber();
@@ -27,7 +32,7 @@ export class HomeComponent {
   }
 
   setPageNumber() {
-    this.pageNumbers=[];
+    this.pageNumbers = [];
     for (let i = 0; i < this.response.totalPageCount; i++) {
       this.pageNumbers.push(i + 1);
 
--- BookStoreServer/BookStoreServer.WebApi/Controllers/BooksController.cs
+++ BookStoreServer/BookStoreServer.WebApi/Controllers/BooksController.cs
@@ -8,13 +8,48 @@ namespace BookStoreServer.WebApi.Controllers;
 [ApiController]
 public class BooksController : ControllerBase
 {
-    private static List<Book> books = new()
-    {
 
-    };
     public BooksController()
     {
-        books = new();
+    }
+
+    [HttpPost]
+    public IActionResult GetAll(RequestDto request)
+    {
+        ResponseDto<List<Book>> response = new();
+        string replaceSearch = request.Search.Replace("İ", "i").ToLower();
+        var newBooks = SeedData.Books
+            .Where(x => x.Title.Replace("İ", "i").ToLower().Contains(replaceSearch) ||
+                       x.Author.Replace("İ", "i").ToLower().Contains(replaceSearch)
+            )
+
+            .ToList();
+
+        response.Data = newBooks.Skip((request.PageNumber - 1) * request.PageSize)
+            .Take(request.PageSize).ToList();
+
+        response.PageNumber = request.PageNumber;
+        response.PageSize = request.PageSize;
+        response.TotalPageCount = (int)Math.Ceiling(newBooks.Count / (double)request.PageSize);
+        response.IsFirstPage = request.PageNumber == 1;
+        response.IsLastPage = request.PageNumber == response.TotalPageCount;
+        return Ok(response);
+    }
+
+
+}
+
+public static class SeedData
+{
+    public static List<Book> Books = new BookService().CreateSeedBookData();
+}
+
+
+public class BookService
+{
+    private List<Book> books = new();
+    public List<Book> CreateSeedBookData()
+    {
         for (int i = 0; i < 100; i++)
         {
             var book = new Book()
@@ -33,29 +68,8 @@ public class BooksController : ControllerBase
             };
             books.Add(book);
         }
+        return books;
     }
 
-    [HttpPost]
-    public IActionResult GetAll(RequestDto request)
-    {
-        ResponseDto<List<Book>> response = new();
-        string replaceSearch = request.Search.Replace("İ", "i").ToLower();
-        response.Data = books
-            .Where(x=> x.Title.Replace("İ","i").ToLower().Contains(replaceSearch) ||
-                       x.Author.Replace("İ","i").ToLower().Contains(replaceSearch)
-            )
-            .Skip((request.PageNumber - 1) * request.PageSize)
-            .Take(request.PageSize)
-            .ToList();
 
-
-        response.PageNumber = request.PageNumber;
-        response.PageSize = request.PageSize;
-        response.TotalPageCount = (int)Math.Ceiling(books.Count / (double)request.PageSize);
-        response.IsFirstPage = request.PageNumber == 1;
-        response.IsLastPage = request.PageNumber == response.TotalPageCount;
-
-
-        return Ok(response);
-    }
 }
