259b2cb07bb557abb2ecafe0f36b2708bfc2394e
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-10-11 23:09:27 +0300

    .

--- BookStoreServer/BookStoreServer.WebApi/Context/AppDbContext.cs
+++ BookStoreServer/BookStoreServer.WebApi/Context/AppDbContext.cs
@@ -14,15 +14,19 @@ public sealed class AppDbContext : DbContext
         optionsBuilder.UseSqlServer("Data Source=TARDEMPC\\SQLEXPRESS;Initial Catalog=BookStoreDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
     }
     public DbSet<Category> Categories { get; set; }
+
     public DbSet<Book> Books { get; set; }
+
     public DbSet<BookCategory> BookCategories { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Book>().OwnsOne(p => p.Price, price =>
         {
+
             price.Property(p => p.Value).HasColumnType("money"); 
             price.Property(p => p.Currency).HasMaxLength(5); 
+
         });
 
         modelBuilder.Entity<BookCategory>().HasKey(p => new { p.BookId, p.CategoryId });
