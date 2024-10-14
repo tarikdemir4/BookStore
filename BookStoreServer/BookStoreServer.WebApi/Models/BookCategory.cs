fc1ccf2f12e82c6a2a4447887ff118e77d79f7be
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-10-14 23:42:30 +0300

    .

--- BookStoreServer/BookStoreServer.WebApi/Models/BookCategory.cs
+++ BookStoreServer/BookStoreServer.WebApi/Models/BookCategory.cs
@@ -2,13 +2,12 @@
 
 public sealed class BookCategory
 {
-
-
     public int BookId { get; set; }
+
     public Book Book { get; set; }
+
     public int CategoryId { get; set; }
+
     public Category Category { get; set; }
-
-
 
 }
